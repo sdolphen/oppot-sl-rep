274eacc8c68e97a19b77d3332a04183164be15f0
sdolphen <stijndolphen@gmail.com>
2024-08-13 19:45:20 +0200

    added api connection to gcp

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Google Sheets setup
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+# Define the scope for Google Sheets API
+scope = ["https://spreadsheets.google.com/feeds", 
+         'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", 
+         "https://www.googleapis.com/auth/drive"]
 
-# You would replace 'credentials.json' with the path to your Google Service Account credentials
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+# Load credentials from Streamlit secrets
+credentials = {
+    "type": st.secrets["google"]["type"],
+    "project_id": st.secrets["google"]["project_id"],
+    "private_key_id": st.secrets["google"]["private_key_id"],
+    "private_key": st.secrets["google"]["private_key"],
+    "client_email": st.secrets["google"]["client_email"],
+    "client_id": st.secrets["google"]["client_id"],
+    "auth_uri": st.secrets["google"]["auth_uri"],
+    "token_uri": st.secrets["google"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["google"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["google"]["client_x509_cert_url"]
+}
+
+# Authenticate and connect to Google Sheets
+creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials, scope)
 client = gspread.authorize(creds)
 
 # Open the Google Sheet
