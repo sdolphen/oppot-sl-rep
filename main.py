54976d8aebb2ab4ebedc0f438a47a031148094a3
sdolphen <stijndolphen@gmail.com>
2024-08-30 18:39:05 +0200

    dbgg

--- main.py
+++ main.py
@@ -59,8 +59,15 @@ We zorgen ervoor dat je hier vanaf zaterdag 14 september zal kunnen reserveren (
 We zullen volgende shiften doen:
 """)
 
-# Adding a smaller banner image
-st.image("oppem-logo.png", width=300)  # Banner is now half as small
+# Centering the image
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <img src="oppem-logo.png" alt="Oppem Logo" width="300">
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
 
 # Updated timeslots
 updated_timeslots = {
