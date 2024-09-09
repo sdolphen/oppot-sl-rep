8ab5cbeccde8dc73c1e9c455b239a1ee1c33d6a7
sdolphen <stijndolphen@gmail.com>
2024-09-09 20:01:06 +0200

    dbg

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ updated_timeslots = {
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("## Zaterdag")
+    st.markdown("#### Zaterdag")
     available_slots_saturday = get_slot_availability("Zaterdag")
     if available_slots_saturday:
         for slot in available_slots_saturday:
@@ -154,7 +154,7 @@ with col1:
         st.info("Alle timeslots voor zaterdag zijn volzet")
 
 with col2:
-    st.markdown("## Zondag")
+    st.markdown("#### Zondag")
     available_slots_sunday = get_slot_availability("Zondag")
     if available_slots_sunday:
         for slot in available_slots_sunday:
